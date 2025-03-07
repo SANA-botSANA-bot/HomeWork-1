258fa903f4b74a525086ba87d1cfbcb00de65c0c
Sana <sanasaki85@gmail.com>
2025-03-07 10:10:56 -0800

    added HomeWork-1

--- 3.py
+++ 3.py
@@ -1,8 +1,52 @@
+"""the first solution for question 3:path parameter."""
+
 from fastapi import FastAPI
-from typing import List
+from typing import List,Optional
 app=FastAPI()
-@app.get("/",response_model=List[int])
-def FirstPlusSecondEqualThirdByFourth():
+@app.get("/num{num}",response_model=List[int])
+def FirstPlusSecondEqualThirdByFourth(num:Optional[int]=None):
+    num=[]
+    for digits in range(1000,10000):
+        str_num=str(digits)
+        if int(str_num[0])+int(str_num[1])==int(str_num[2])*int(str_num[3]):
+            num.append(digits)
+    return num
+
+
+
+
+
+
+"""the second solution for question 3:query parameters."""
+
+from fastapi import FastAPI
+from typing import List,Optional
+app=FastAPI()
+@app.get("/num",response_model=List[int])
+def FirstPlusSecondEqualThirdByFourth(num:Optional[int]=None):
+    num=[]
+    for digits in range(1000,10000):
+        str_num=str(digits)
+        if int(str_num[0])+int(str_num[1])==int(str_num[2])*int(str_num[3]):
+            num.append(digits)
+    return num
+
+
+
+
+
+
+
+"""the third solution for question 3:request body."""
+
+from fastapi import FastAPI
+from typing import List,Optional
+from pydantic import BaseModel
+app=FastAPI()
+class result(BaseModel):
+    num:int
+@app.post("/num",response_model=List[int])
+def FirstPlusSecondEqualThirdByFourth(num:Optional[int]=None):
     num=[]
     for digits in range(1000,10000):
         str_num=str(digits)
--- 4.py
+++ 4.py
@@ -1,8 +1,54 @@
-from fastapi import FastAPI,Path
+"""the first solution for question 4:path parameter."""
+
+from fastapi import FastAPI
 from typing import List
 app=FastAPI()
-@app.get("/",response_model=List[int])
+@app.get("/number/{EvenNumbers}",response_model=List[int])
 def PrintAllEvenDigits():
+    EvenNumbers=[]
+    for number in range(100,1000,2):
+        str_number=str(number)
+        if int(str_number[0])%2==0 and int(str_number[1])%2==0 and int(str_number[2])%2==0:
+            EvenNumbers.append(number)
+    return EvenNumbers
+
+
+
+
+
+
+"""the second solution for question 4:query parameters."""
+
+
+from fastapi import FastAPI
+from typing import List,Optional
+app=FastAPI()
+@app.get("/number/",response_model=List[int])
+def PrintAllEvenDigits(EvenNumbers:Optional[int]=None):
+    EvenNumbers=[]
+    for number in range(100,1000,2):
+        str_number=str(number)
+        if int(str_number[0])%2==0 and int(str_number[1])%2==0 and int(str_number[2])%2==0:
+            EvenNumbers.append(number)
+    return EvenNumbers
+
+
+
+
+
+
+
+
+"""the third solution for question 4:request body."""
+
+from fastapi import FastAPI
+from typing import List,Optional
+from pydantic import BaseModel
+app=FastAPI()
+class number(BaseModel):
+    EvenNumbers:int
+@app.post("/number/",response_model=List[int])
+def PrintAllEvenDigits(EvenNumbers:Optional[int]=None):
     EvenNumbers=[]
     for number in range(100,1000,2):
         str_number=str(number)
